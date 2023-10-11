c21d3d1bb1c5141d175150a72e7b8855ac9201e4
Danil-114195722 <ejyou.user@gmail.com>
2023-10-11 18:37:17 +0300

    add one-line solution and solution with half division

---
+++ PZ_2_20/half_division.py
@@ -0,0 +1,25 @@
+"""
+Разработать программу, выводящую на экран число, полученное при перестановке цифр сотен и десятков исходного числа.
+"""
+
+
+def main() -> None:
+    try:
+        str_num = int(input('Введите трёхзначное число: '))
+
+        # если юзер ввёл НЕ трёхзначное число
+        if len(str(str_num)) != 3:
+            raise ValueError
+
+        int_num = int(str_num)
+        result = int_num // 10 % 10 * 100 + int_num // 100 * 10 + int_num % 10
+        print(f'Ответ: {result}')
+
+    # обработка ошибок
+    except ValueError:
+        print('Неверные данные!!!')
+        return
+
+
+if __name__ == "__main__":
+    main()
