62ff6866c28f28dca85c4f34e52b8868d4e03307
Danil-114195722 <ejyou.user@gmail.com>
2023-11-11 22:16:44 +0300

    Finished PZ_14 and PZ_17

--- PZ_14_8/PZ_14_8.py
+++ PZ_14_8/PZ_14_8.py
@@ -1,11 +1,18 @@
+from re import findall
+
+
 def task():
     """
     В исходном текстовом файле(Dostoevsky.txt) найти все произведения писателя.
     Посчитать количество полученных элементов.
     """
 
-    print('cnrwejhvbej')
+    with open('Dostoevsky.txt', 'r') as bio_file:
+        bio = bio_file.read()
+    all_works = set(findall('«[А-ЯЁ][\w\s]*»', bio))
+
+    print('Все произведения:', ', '.join(all_works))
+    print('Кол-во произведений:', len(all_works))
 
 
-if __name__ == '__main__':
-    task()
+task()
--- PZ_17_8/PZ_17_8.py
+++ PZ_17_8/PZ_17_8.py
@@ -6,9 +6,48 @@
 """
 
 
-class A:
-    pass
+class Animal:
+    def __init__(self, nickname: str, sex: str, old: int):
+        self.nickname = nickname
+        self.sex = sex
+        self.old = old
+
+    def breathe(self):
+        print(f'{self.nickname} дышит')
+
+    def eat(self):
+        print(f'{self.nickname} питается')
+
+    def get_info(self):
+        print(f'Кличка: {self.nickname}\nПол: {self.sex}\nВозраст: {str(self.old)}')
+
+
+class Dog(Animal):
+    def __init__(self, nickname: str, sex: str, old: int):
+        super().__init__(nickname, sex, old)
+
+    def bark(self):
+        print(f'{self.nickname} гавкает')
+
+
+class Cat(Animal):
+    def __init__(self, nickname: str, sex: str, old: int):
+        super().__init__(nickname, sex, old)
+
+    def purr(self):
+        print(f'{self.nickname} мурлычет')
 
 
 if __name__ == '__main__':
-    a = A()
+    my_dog = Dog(nickname='Шарик', sex='Самец', old=8)
+    my_cat = Cat(nickname='Матроскин', sex='Самец', old=5)
+
+    my_cat.get_info()
+    print()
+    my_dog.get_info()
+    print()
+
+    my_dog.breathe()
+    my_cat.purr()
+    my_dog.bark()
+    my_cat.eat()
