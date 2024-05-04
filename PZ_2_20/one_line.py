ae3ab2eb32d4f552e6cf8e791443e28f9b3296b8
Ej_you <113062021+Danil-114195722@users.noreply.github.com>
2024-05-04 09:39:55 +0300

    Delete PZ_2_20/one_line.py

--- PZ_2_20/one_line.py
+++
@@ -1,1 +0,0 @@
-print((lambda num: f'Ответ: {num[1] + num[0] + num[2]}' if (num.isdigit() and len(num) == 3) else 'Неверные данные')(input('Введите трёхзначное число: ')))  # 2 PZ в 1 строку для крутых
